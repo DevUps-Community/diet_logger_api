4dd12589271323c0f0bb3e4d22646d04433fd7d7
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-06-06 20:55:33 +0100

    adjust sql file

--- db/setup.sql
+++ db/setup.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS diet_logger;
-CREATE DATABASE diet_logger;
-
 DROP DATABASE IF EXISTS diet_logger_test;
+DROP DATABASE IF EXISTS diet_logger;
+
 CREATE DATABASE diet_logger_test;
+CREATE DATABASE diet_logger;
